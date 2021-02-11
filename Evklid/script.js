43f6e2bae4d9c11255f281c8ca705c691bd771b0
Laranto-spb <laranto1985@gmail.com>
2021-02-11 11:47:42 +0300

    feat: Add tabs to Evklid working section

--- Evklid/script.js
+++ Evklid/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // Swiper for hero
 
   const swiper = new Swiper('.swiper-container', {
     // Optional parameters
@@ -7,9 +8,37 @@ window.addEventListener('DOMContentLoaded', () => {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      type: 	'bullets',
+      type: 'bullets',
       clickable: true,
     },
   });
+
+  //Tabs for working 
+
+  document.querySelectorAll('.tabs-btn').forEach(function (tabsBtn) {
+    tabsBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path;
+
+      document.querySelectorAll('.working__more').forEach(function (tabContent) {
+        tabContent.classList.remove('working__more-active');
+      })
+
+      document.querySelector(`[data-target="${path}"]`).classList.add('working__more-active');
+
+      document.querySelectorAll('.tabs-btn').forEach(function (tabs) { // When clicked removes active (color) from all btns
+        tabs.classList.remove('working__link-active');
+      })
+
+      tabsBtn.classList.add('working__link-active'); // Adds color to clicked btn
+
+    })
+
+
+
+  })
+
+
+
+
 
 })
